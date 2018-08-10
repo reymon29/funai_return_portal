6a61c0a3eb7e0cfee2a171785b3d7caa89829580
Reyes Montemayor <sjpete1234@hotmail.com>
2018-08-11 02:23:37 +0900

    generated model and views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,7 +13,7 @@ class ApplicationController < ActionController::Base
   end
 
   def after_sign_in_path_for(resource)
-    gratitudes_path
+    dashboard_path
   end
 
   def after_sign_out_path_for(resource_or_scope)
--- app/controllers/returns_controller.rb
+++ app/controllers/returns_controller.rb
@@ -1,17 +1,37 @@
 class ReturnsController < ApplicationController
-
+  before_action :return_id_find, only: [:show, :update]
   def index
+    @returns = Return.all
   end
 
   def new
+    @return = Return.new
   end
 
   def create
+    @return = Return.new(return_params)
+    @return.user = current_user
+    if @return.save
+      redirect_to dashboard_path
+      flash[:notice] = "Return for Aaron's #{@return.item_number} has been submitted and will be reviewed shortly"
+    else
+      render :new
+    end
   end
 
   def show
   end
 
   def update
+  end
+
+  private
+
+  def return_id_find
+    @return = Return.find(params[:id])
+  end
+
+  def return_params
+    params.require(:return).permit(:item_number, :model_number, :serial_number, :invoice_date, :lease_date, :part_number, :return_reason, :comment)
   end
 end
