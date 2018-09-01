b81d5fe667666dd88887e3d000dc367c8c46eab6
Reyes Montemayor <sjpete1234@hotmail.com>
2018-09-02 00:15:42 +0900

    fixed small javascript bug

--- app/javascript/components/tab.js
+++ app/javascript/components/tab.js
@@ -1,15 +1,17 @@
 function tabs() {
   console.log('Hi')
-  var header = document.getElementById("nav-id");
-  var btns = header.getElementsByClassName("tab");
-  for (var i = 0; i < btns.length; i++) {
-    btns[i].addEventListener("click", function() {
-      var current = document.getElementsByClassName("active");
-      if (current.length > 0) {
-        current[0].className = current[0].className.replace(" active", "");
-      }
-      this.className += " active";
-    });
+  if (document.getElementById('nav-id')) {
+    var header = document.getElementById("nav-id");
+    var btns = header.getElementsByClassName("tab");
+    for (var i = 0; i < btns.length; i++) {
+      btns[i].addEventListener("click", function() {
+        var current = document.getElementsByClassName("active");
+        if (current.length > 0) {
+          current[0].className = current[0].className.replace(" active", "");
+        }
+        this.className += " active";
+      });
+    }
   }
 }
 
