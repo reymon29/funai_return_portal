828a6985bb8227d79ca33615c2c3bee330295ea8
Reyes Montemayor <sjpete1234@hotmail.com>
2018-09-03 14:49:36 +0900

    Created updated email

--- app/mailers/return_mailer.rb
+++ app/mailers/return_mailer.rb
@@ -8,6 +8,6 @@ class ReturnMailer < ApplicationMailer
 
   def updated(return_item)
     @return = return_item
-    mail(to: @return.user.email, subject: "Funai return updated-#{@return.item_number}-#{@return.product.model_number}")
+    mail(to: @return.user.email, cc: "funaicontact@gmail.com" ,subject: "Funai return updated-#{@return.item_number}-#{@return.product.model_number}")
   end
 end
