75c76d2ccb09b6f502cdc66825931534d0f7337e
Reyes Montemayor <sjpete1234@hotmail.com>
2018-09-07 01:23:58 +0900

    cleaned up controller for admin

--- app/admin/products.rb
+++ app/admin/products.rb
@@ -1,6 +1,6 @@
 ActiveAdmin.register Product do
   permit_params :model_number, :product_type, :weight, :carrier_default, :enable
-
+  actions :index, :show, :new, :create, :update, :edit
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
@@ -14,26 +14,7 @@ ActiveAdmin.register Product do
 #   permitted << :other if params[:action] == 'create' && current_user.admin?
 #   permitted
 # end
-  controller do
-    def new
-      @product = Product.new
-    end
 
-    def create
-      @product = Product.new
-      @product.model_number = params[:product][:model_number]
-      @product.product_type = params[:product][:product_type]
-      @product.carrier_default = params[:product][:carrier_default]
-      @product.enable = params[:product][:enable]
-      @product.weight = params[:product][:weight].to_i
-      if @product.save
-        redirect_to admin_product_path(@product)
-        flash[:alert] = "Saved"
-      else
-        flash[:alert] = "Did not save please try again"
-      end
-    end
-  end
 
   form do |f|
     f.semantic_errors # shows errors on :base
