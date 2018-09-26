5aa9d56d404b8925bb7295c0296655d1b25e24a4
Reyes Montemayor <sjpete1234@hotmail.com>
2018-09-26 22:55:48 +0900

    setup location type

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,6 +16,7 @@ class User < ApplicationRecord
   validates :last_name, presence: true
   validates :phone_number, presence: true, format: { with: /^[0-9]{10}$/, multiline: true,
     message: "format 5555555555" }
+  validates :location_type, presence: true
   after_create :send_welcome_email
 
   # Active admin name
