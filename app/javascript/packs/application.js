cce26730bc336759546254d5ae8a88e3665777a8
Reyes Montemayor <sjpete1234@hotmail.com>
2018-09-30 20:21:27 +0900

    added flatpickr

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,18 +1,11 @@
 import "bootstrap";
+import "../plugins/flatpickr";
+
 import { loadDynamicBannerText } from '../components/banner';
 loadDynamicBannerText();
 
 import { tabs } from '../components/tab';
 tabs();
 
-if (document.querySelector('.container-svg')) {
-  const btnSave = document.querySelector(".btn-primary");
-  const defaultSVG = document.querySelector(".container-svg");
-  const defaultFlex = document.querySelector(".flex-container");
-  const defaultAlert = document.querySelector(".alert");
-  btnSave.addEventListener('click', (event) => {
-    defaultFlex.style.display = "none";
-    defaultSVG.style.display = "initial";
-  });
-}
+
 
---
+++ app/javascript/plugins/flatpickr.js
@@ -0,0 +1,10 @@
+import flatpickr from "flatpickr"
+import "flatpickr/dist/themes/airbnb.css"; // Note this is important!
+
+
+flatpickr(".datepicker", {
+  altInput: true
+});
+
+
+
