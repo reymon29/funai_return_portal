02aeaf0fc7594540640d485b152f2cda6b607058
Reyes Montemayor <sjpete1234@hotmail.com>
2018-10-08 20:06:15 +0900

    modified:   app/models/user.rb 	modified:   config/application.rb 	modified:   config/environments/development.rb 	modified:   config/environments/production.rb

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,8 @@ module FunaiReturnPortal
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
     config.assets.initialize_on_precompile = false
+    config.action_mailer.delivery_method = :postmark
+    config.action_mailer.postmark_settings   = { api_key: ENV['POSTMARK_API_KEY'] }
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
