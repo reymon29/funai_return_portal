7d116921b38a40688cc9c47bef6f32e1a6fe4ea4
Reyes Montemayor <sjpete1234@hotmail.com>
2018-10-18 14:30:09 +0900

    setup image preview and remove images btn

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -5,3 +5,41 @@ import { tabs } from '../components/tab';
 tabs();
 import "../plugins/flatpickr"
 
+$(function(){
+  $(".form-control-file").on("change", function(){
+    var preview = document.querySelector('#preview');
+    var files   = document.querySelector('input[type=file]').files;
+
+    function readAndPreview(file) {
+
+      if ( /\.(jpe?g|png|gif)$/i.test(file.name) ) {
+        var reader = new FileReader();
+
+        reader.addEventListener("load", function () {
+          var image = new Image();
+          image.height = 100;
+          image.width = 100;
+          image.title = file.name;
+          image.src = this.result;
+          preview.appendChild( image );
+        }, false);
+
+        reader.readAsDataURL(file);
+      }
+
+    }
+
+    if (files) {
+      [].forEach.call(files, readAndPreview);
+    }
+  })
+})
+
+$('#btn-example-file-reset').on('click', function(e){
+   var $el = $('#return_images');
+   var $el2 = $('#preview');
+   $el.wrap('<form>').closest('form').get(0).reset();
+   $el.unwrap();
+   $el2.empty();
+
+});
