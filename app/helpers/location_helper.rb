5b99b3e7f9bd7924e88f874270bea52d921ce28f
Reyes Montemayor <sjpete1234@hotmail.com>
2018-10-27 12:57:22 +0900

    removed helper that are no longer being used

--- app/helpers/location_helper.rb
+++
@@ -1,7 +0,0 @@
-module LocationHelper
-  def location_type
-    [ ["Store", "store_front"],
-      ["Fulfillment Center", "fulfillment_center"],
-      ["Service Center", "service_center"]]
-  end
-end
