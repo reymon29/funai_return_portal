f53edcd4add7613fb9a6ba88f66b3390be963396
Reyes Montemayor <sjpete1234@hotmail.com>
2018-11-05 17:39:39 +0900

    enable view

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -4,9 +4,7 @@ class AdminMailer < ApplicationMailer
     @return_pending = Return.pending_approval
     @return_fedex = Return.fedex_product
     @return_tsg = Return.ltl_product
-    # mail( to: "rmontemayor@funaiservice.com, nhart@funaiservice.com, kchen@funaiservice.com, aguerra@funaicorp.com, ANguyen@funaicorp.com, cgibson@funaiservice.com",
-    # subject: "Funai Pending Stats")
-    mail( to: "rmontemayor@funaiservice.com",
-    subject: "Funai Pending Stats")
+    mail( to: "rmontemayor@funaiservice.com, nhart@funaiservice.com, kchen@funaiservice.com, aguerra@funaicorp.com, ANguyen@funaicorp.com, cgibson@funaiservice.com",
+    subject: "9am Daily email Funai Team-Aaron's Pending items")
   end
 end
