6482bc15d46eaba62aaf3a9cf9b1de6aa80549ba
Reyes Montemayor <sjpete1234@hotmail.com>
2019-03-25 12:46:17 +0900

    update development for security alert

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,6 +59,8 @@ Rails.application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+  #
+  config.secret_key_base = SecureRandom.hex(64)
 
   # Suppress logger output for asset requests.
   config.assets.quiet = true
