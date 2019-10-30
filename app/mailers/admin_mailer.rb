b2f744f0a6f2c33041ee7f027ff46feae74a88f7
Reyes Montemayor <sjpete1234@hotmail.com>
2019-10-30 07:15:22 -0400

    editing email dist. list

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -4,7 +4,7 @@ class AdminMailer < ApplicationMailer
     @return_pending = Return.pending_approval
     @return_fedex = Return.fedex_product
     @return_tsg = Return.ltl_product
-    mail( to: "rmontemayor@funaiservice.com, nhart@funaiservice.com, lwang@funaiservice.com, kchen@funaiservice.com, aguerra@funaicorp.com, ANguyen@funaicorp.com, jjaimes@funaicorp.com, cgibson@funaiservice.com",
+    mail( to: "rmontemayor@funaiservice.com, nhart@funaiservice.com, lwang@funaiservice.com, kchen@funaiservice.com, ANguyen@funaicorp.com, jjaimes@funaicorp.com, cgibson@funaiservice.com",
     subject: "Daily Email Funai Team-Aaron's Pending items")
   end
 end
