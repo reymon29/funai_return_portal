72b911af2e0acd945aa1c2f5643a9505e5d06d23
Reyes Montemayor <sjpete1234@hotmail.com>
2020-04-23 11:56:57 -0400

    Adding item number to the csv download in active admin

--- app/admin/return_logs.rb
+++ app/admin/return_logs.rb
@@ -1,2 +1,9 @@
 ActiveAdmin.register ReturnLog do
+  csv do
+    column :id
+    column (:return_item_number) {|returnlog| returnlog.return.item_number}
+    column :comment
+    column :created_at
+    column :updated_at
+  end
 end
